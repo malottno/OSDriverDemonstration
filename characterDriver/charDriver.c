5d51f2456a1e2659ae3fc20e8c0d5bf6767d7030
nickmal <nick@malott.us>
2017-10-30 17:35:41 -0400

    Updates to character driver for streaming support

--- characterDriver/charDriver.c
+++ characterDriver/charDriver.c
@@ -7,9 +7,11 @@
 
 MODULE_LICENSE("GPL");
 
-static struct cdev charDevice;
+int characterCount = 0;
+
+//static struct cdev charDevice;
 static DEFINE_MUTEX(charLock);
-static char *inFile = "/dev/tty0";
+static char *inFile = "/dev/tty10";
 static char *outFile = "./driverOutput";
 
 module_param(inFile,charp,0000);
@@ -18,23 +20,27 @@ module_param(outFile,charp,0000);
 MODULE_PARM_DESC(inFile, "File path for input to character driver");
 MODULE_PARM_DESC(outFile, "File path for output from character driver");
 
+static int major = 0;
+static const char device[] = "charDriver-Example"; 
 static ssize_t c_read(struct file *f, char __user *buf, size_t len, loff_t *off);
 static ssize_t c_write(struct file *f, const char __user *buf, size_t len, loff_t *off);
 static int c_open(struct inode *i, struct file *f);
 
 static ssize_t c_read(struct file *f, char __user *buf, size_t len, loff_t *off){
 	ssize_t retVal = 0;
+	printk(KERN_INFO "charDriver: Read()\n");
 	return retVal;
 }
 
 static ssize_t c_write(struct file *f, const char __user *buf, size_t len, loff_t *off){
 	ssize_t retVal = 0;
+	printk(KERN_INFO "charDriver: Write()\n");
 	return retVal;
 }
 
 static int c_open(struct inode *i, struct file *f){
 	int retVal = 0;
-	printk(KERN_INFO "\tChar driver opened");
+	printk(KERN_INFO "\tcharDriver: opened");
 	return retVal;
 }
 
@@ -48,7 +54,7 @@ static struct file_operations fops={
 
 int init_module(void){
 	printk(KERN_ALERT "Entering Character Driver\n");
-	cdev_init(&charDevice, &fops);
+	major = register_chrdev(0, device, &fops);
 	//Entering code
 	printk(KERN_ALERT "\tinFile: %s\n",inFile);
 	printk(KERN_ALERT "\toutFile: %s\n", outFile);
@@ -64,6 +70,9 @@ int init_module(void){
 void cleanup_module(void){
 	//Closing code
 	printk(KERN_ALERT "Closing Character Driver\n");
+	if(major != 0){
+		unregister_chrdev(major, device);
+	}
 	return;
 }
 
